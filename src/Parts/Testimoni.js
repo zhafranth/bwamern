082ea946dc405b259573dddf91fceb29ee84ca64
Zhafranth <zhafranth@gmail.com>
2020-05-07 23:57:02 +0700

    add style Testimonial

--- src/Parts/Testimoni.js
+++ src/Parts/Testimoni.js
@@ -26,7 +26,7 @@ export default function Testimoni({ data }) {
             />
           </div>
         </div>
-        <div className="col-6">
+        <div className="col">
           <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
           <Star value={3.9} widht={35} height={35} spacing={4}></Star>
           <h5 className="h2 font-weight-light line-hight-2 my-3">
@@ -35,17 +35,18 @@ export default function Testimoni({ data }) {
           <span className="text-gray-500">
             {data.familyName}, {data.familyOccupation}
           </span>
-
-          <Button
-            type="link"
-            isPrimary
-            hasShadow
-            className="btn px-5"
-            style={{ marginTop: 40 }}
-            href={`/testimonial/${data._id}`}
-          >
-            Read Their Story
-          </Button>
+          <div>
+            <Button
+              type="link"
+              isPrimary
+              hasShadow
+              className="btn px-5"
+              style={{ marginTop: 40 }}
+              href={`/testimonial/${data._id}`}
+            >
+              Read Their Story
+            </Button>
+          </div>
         </div>
       </div>
     </section>
