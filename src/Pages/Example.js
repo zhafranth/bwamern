021e59875db48375761be42b20c1fe8c6a074a3b
Zhafranth <zhafranth@gmail.com>
2020-05-12 15:42:09 +0700

    add Breadcrumb component

--- src/Pages/Example.js
+++ src/Pages/Example.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import { InputDate, InputNumber } from "elements/Form";
+import Breadcrumb from "elements/Breadcrumb";
+// import { InputDate, InputNumber } from "elements/Form";
 
 export default class Example extends Component {
-  state = {
-    value: {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: "selection",
-    },
-  };
+  // state = {
+  //   value: {
+  //     startDate: new Date(),
+  //     endDate: new Date(),
+  //     key: "selection",
+  //   },
+  // };
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  // handleChange = (e) => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
   render() {
+    const breadcrumb = [
+      { pageTitle: "home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
     return (
       <div className="container">
         <div
@@ -21,12 +26,7 @@ export default class Example extends Component {
           style={{ height: "100vh" }}
         >
           <div className="col-auto">
-            <InputDate
-              max={30}
-              onChange={this.handleChange}
-              name="value"
-              value={this.state.value}
-            />
+            <Breadcrumb data={breadcrumb}></Breadcrumb>
           </div>
         </div>
       </div>
